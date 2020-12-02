d1a135578e9c6566e0ef5699b30bff3d3a3c1cd0
John Bird <john.bird@colorado.edu>
2020-12-02 16:55:15 -0700

    added a three d particle

--- pybots/src/simulation/particle.py
+++ pybots/src/simulation/particle.py
@@ -62,3 +62,70 @@ class DynamicParticle(simulation.dynamic_system.DynamicSystem):
         """
         return self._X[2:4]
 
+class ThreeDParticle(simulation.dynamic_system.DynamicSystem):
+    """Implements the dynamics to simulate a particle with dynamics in 3d"""
+    def __init__(
+        self,
+        X0,
+        dt,
+        output_fcn=None,
+        input_fcn=None):
+        """Constructor
+
+        Arguments:
+            X0: initial state
+                x: x position
+                y: y position
+                z: z position
+                vx: x velocity
+                vy: y velocity
+                vz: z velocity
+            dt: time step size
+            output_fcn: output function, takes X and returns Y
+            input_fcn: a function to determine input from the state
+                for example a controller
+
+        Returns:
+            object instance
+        """
+        super(DynamicParticle, self).__init__(
+            X0, dt, self._dynamics, output_fcn, input_fcn)
+        self._U = numpy.zeros((3,))
+
+    def _dynamics(self, X, U):
+        """Compute the system dynamics
+
+        Arguments:
+            X: state
+                x: x position
+                y: y position
+                z: z position
+                vx: x velocity
+                vy: y velocity
+                vz: z velocity
+            U: input
+                ax: x acceleration
+                ay: y acceleration
+                az: z acceleration
+        """
+        xdot = X[3]
+        ydot = X[4]
+        zdot = X[5]
+        vxdot = U[0]
+        vydot = U[1]
+        vzdot = U[2]
+        X_dot = numpy.array([xdot, ydot, zdot, vxdot, vydot, vzdot])
+        return X_dot
+
+    @property
+    def position(self):
+        """Get the position
+        """
+        return self._X[0:3]
+
+    @property
+    def velocity(self):
+        """Get the heading
+        """
+        return self._X[3:6]
+
